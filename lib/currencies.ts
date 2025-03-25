ad8d52163ae82f968ee0e67e4ad9dbe6bf50033e
Mohammad Umair <74056295+M-UmairTariq@users.noreply.github.com>
2025-03-26 00:43:28 +0500

    Added Prisma ORM and Implemented Wizard Page

---
+++ lib/currencies.ts
@@ -0,0 +1,6 @@
+export const Currencies = [
+    {value: "PKR" , label: "Rs - PKR", locale: "en-PK"},
+    {value: "USD", label: "$ - USD" , locale: "en-US"},
+];
+
+export type Currency = (typeof Currencies)[0];
